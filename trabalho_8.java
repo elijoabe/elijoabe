caf7d9ca42d4b517bde3cbd6f9391c2c71525c22
elijoabe <104981464+elijoabe@users.noreply.github.com>
2022-09-21 22:01:43 -0300

    Add files via upload

---
+++ trabalho_8.java
@@ -0,0 +1,28 @@
+import java.util.Scanner ;
+
+public class Main {
+ 
+public public static void main(String[] args) {
+    Scanner se = new Scanner (System.in);
+    float gasol, etanol, eficiencia;
+    System.out.printf(format:" A cotação valor etanol?%n");
+    etanol = sc.nextFloat();
+    System.out.printf(format:"A cotação valor gasolina?%n");
+    gasol = sc.nextFloat();
+    eficiencia = (gasol * 70/100);
+    if (eficiencia > etanol){
+        System.out.printf(format:"Se compreende mais eficiente o etanol !%n");
+    } else
+    {
+        System.out.printf(format:"Se compreende eficiente com gasolina !%n");
+}
+    sc.close();
+      }
+
+    
+  
+
+
+
+
+}
